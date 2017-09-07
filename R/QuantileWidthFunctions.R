88b79331c9402a90e01e4977364e91ee26abebc7
Charles Plessy <plessy@riken.jp>
2017-09-07 17:03:15 +0900

    change .get.quant.pos() to work with GRanges.

--- R/QuantileWidthFunctions.R
+++ R/QuantileWidthFunctions.R
@@ -1,37 +1,41 @@
-#####
-# Function that calculates position of quantiles for CTSS clusters based on distribution of tpm within the cluster
-# ARGUMENTS: cluster.cumsums - named list of vectors containing cumulative sum for each cluster (returned by 'get.cumsum' function)
-#            coors - data frame of clusters containing least one column, *start (genomic start position of the cluster)
-#			 q.low - desired lower quantile - single value or a vector of values
-#            q.up - desired upper quantile - single value or a vector of values (must be the same length as q.low)
-# RETURNS: 
+#' @name .get.quant.pos
+#' 
+#' @rdname QuantileWidthFunctions
+#' 
+#' @title Get quantile positions
+#' 
+#' @description Function that calculates position of quantiles for CTSS clusters based on
+#' distribution of tpm within the cluster
+#' 
+#' @param cumsums Named list of vectors containing cumulative sum for each cluster
+#'        (returned by 'get.cumsum' function).
+#' @param clusters GRanges object representing tag clusters or consensus clusters.
+#' @param q desired quantiles - single value or a vector of values.
+#' @param useMulticore,nrCores See \code{quantilePositions}.
+#' 
+#' @return Returns the \code{clusters} object with one more metadata column per value
+#' in \code{q}, containing Rle integers giving the relative distance of the quantile
+#' boundaries to the start position.
+#' 
+#' @examples 
+#' \dontrun{ #because it runs through quantilePositions() anyway.
+#' ce       <- readRDS(system.file(package = "CAGEr", "extdata/CAGEexp.rds"))
+#' cumsums  <- CTSScumulativesTagClusters(object, 1)
+#' clusters <- tagClustersGR(object, 1)
+#' .get.quant.pos(cumsums, clusters, c(.1, .9))
+#' }
 
+.get.quant.pos <- function(cumsums, clusters, q = NULL, useMulticore = FALSE, nrCores = NULL) {
+  getQuantilepos <- function(quant, cumsum) length(Filter(isTRUE, cumsum/max(cumsum) < quant))
 
-.get.quant.pos <- function(cluster.cumsums, coors, q = NULL, useMulticore = FALSE, nrCores = NULL) {
-	
-  getQuantilepos <- function(quant, cumsum) length(Filter(isTRUE, cumsum/max(cumsum) < quant)) + 1
-  useMulticore <- .checkMulticore(useMulticore)
-
-	if(!is.null(q)) {
-		if(useMulticore == TRUE){
-			cluster.q = mclapply(cluster.cumsums, function(x) {sapply(q, getQuantilepos, x)}, mc.cores = .getNrCores(nrCores))
-		}else{
-			cluster.q = lapply(cluster.cumsums, function(x) {sapply(q, getQuantilepos, x)})
-		}
-		cluster.q = do.call(rbind, cluster.q)
-		if(class(cluster.q[1,1]) == 'list') {
-			un = unlist(lapply(cluster.q, function(x) {length(unlist(x))}))
-			l = lapply(cluster.q, function(x) {unlist(x)})
-			a1 = do.call(rbind, l[which(un !=0)])
-			cluster.q = as.data.frame(matrix(rep(coors$start[which(coors$tpm>0)], ncol(cluster.q)), ncol = ncol(cluster.q), byrow = F) -1 + as.matrix(a1))
-		}else{
-			cluster.q = as.data.frame(matrix(rep(coors$start[which(coors$tpm>0)], ncol(cluster.q)), ncol = ncol(cluster.q), byrow = F) -1 + cluster.q)
-		}
-		colnames(cluster.q) = paste('q_', q, sep = '')
-		cluster.q = cbind(cluster = as.integer(names(cluster.cumsums)[which(coors$tpm>0)]), cluster.q)
-		cluster.q = merge(coors, cluster.q, all.x = T, all.y = F)
+	if (.checkMulticore(useMulticore)) {
+		cluster.q <- mclapply( cumsums, function(x) sapply(q, getQuantilepos, x)
+		                     , mc.cores = .getNrCores(nrCores))
+	} else {
+		cluster.q <-   lapply(cumsums, function(x) sapply(q, getQuantilepos, x))
 	}
-		
-	return(cluster.q[order(cluster.q$cluster),])	
-	
+	cluster.q <- as.data.frame(do.call(rbind, cluster.q))
+	colnames(cluster.q) = paste('q_', q, sep = '')
+	mcols(clusters) <- cbind(mcols(clusters), DataFrame(lapply(cluster.q, Rle)))
+	clusters
 }
