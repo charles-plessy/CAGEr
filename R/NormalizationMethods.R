b07725cab4a9e0ae2d5c84c594aeb009a3decd9f
Charles Plessy <plessy@riken.jp>
2018-06-19 14:14:14 +0900

    Add regression tests to normalizeTagCount.
    Also markdownise the markup.

--- R/NormalizationMethods.R
+++ R/NormalizationMethods.R
@@ -12,68 +12,70 @@
 #' 
 #' @description Normalizes raw CAGE tag count per CTSS in all experiments to a same
 #' referent distribution.  A simple tag per million normalization or normalization to a
-#' referent power-law distribution (Balwierz \emph{et al}., Genome Biology 2009) can be
+#' referent power-law distribution (Balwierz _et al_., Genome Biology 2009) can be
 #' specified.
 #' 
-#' @param object A \code{\link{CAGEset}} object
+#' @param object A [`CAGEset`] object
 #' 
-#' @param method Method to be used for normalization.  Can be either \code{"simpleTpm"}
-#'   to convert tag counts to tags per million or \code{"powerLaw"} to normalize to a
-#'   referent power-law distribution, or \code{"none"} to keep using the raw tag counts
+#' @param method Method to be used for normalization.  Can be either `"simpleTpm"`
+#'   to convert tag counts to tags per million or `"powerLaw"` to normalize to a
+#'   referent power-law distribution, or `"none"` to keep using the raw tag counts
 #'   in downstream analyses.
 #' 
 #' @param fitInRange An integer vector with two values specifying a range of tag count
 #'   values to be used for fitting a power-law distribution to reverse cumulatives.
-#'   Used only when \code{method = "powerLaw"}, otherwise ignored.  See Details.
+#'   Used only when `method = "powerLaw"`, otherwise ignored.  See Details.
 #' 
 #' @param alpha	\code{-1 * alpha} will be the slope of the referent power-law distribution
-#'   in the log-log representation.  Used only when \code{method = "powerLaw"}, otherwise
+#'   in the log-log representation.  Used only when `method = "powerLaw"`, otherwise
 #'   ignored.  See Details.
 #'   
 #' @param T Total number of CAGE tags in the referent power-law distribution.  Setting
 #'   \code{T = 10^6} results in normalized values that correspond to tags per million in
-#'   the referent distribution.  Used only when \code{method = "powerLaw"}, otherwise
+#'   the referent distribution.  Used only when `method = "powerLaw"`, otherwise
 #'   ignored.  See Details.
 #' 
 #' @details It has been shown that many CAGE datasets follow a power-law distribution
-#' (Balwierz \emph{et al}., Genome Biology 2009).  Plotting the number of CAGE tags
+#' (Balwierz _et al_., Genome Biology 2009).  Plotting the number of CAGE tags
 #' (X-axis) against the number of TSSs that are supported by >= of that number of tags
 #' (Y-axis) results in a distribution that can be approximated by a power-law.  On a
 #' log-log scale this theoretical referent distribution can be described by a
-#' monotonically decreasing linear function \code{y = -1 * alpha * x + beta}, which is
-#' fully determined by the slope \code{alpha} and total number of tags \code{T} (which
-#' together with \code{alpha} determines the value of \code{beta}).  Thus, by specifying
-#' parameters \code{alpha} and \code{T} a desired referent power-law distribution can be
+#' monotonically decreasing linear function `y = -1 * alpha * x + beta`, which is
+#' fully determined by the slope `alpha` and total number of tags `T` (which
+#' together with `alpha` determines the value of `beta`).  Thus, by specifying
+#' parameters `alpha` and `T` a desired referent power-law distribution can be
 #' selected.  However, real CAGE datasets deviate from the power-law in the areas of very
 #' low and very high number of tags, so it is advisable to discard these areas before
-#' fitting a power-law distribution.  \code{fitInRange} parameter allows to specify a
+#' fitting a power-law distribution. `fitInRange` parameter allows to specify a
 #' range of values (lower and upper limit of the number of CAGE tags) that will be used to
-#' fit a power-law.  Plotting reverse cumulatives using \code{\link{plotReverseCumulatives}}
+#' fit a power-law.  Plotting reverse cumulatives using [`plotReverseCumulatives`]
 #' function can help in choosing the best range of values.  After fitting a power-law
 #' distribution to each CAGE dataset individually, all datasets are normalized to a
-#' referent distribution specified by \code{alpha} and \code{T}. When \code{T = 10^6},
+#' referent distribution specified by `alpha` and `T`. When `T = 10^6`,
 #' normalized values are expressed as tags per million (tpm).
 #' 
-#' @return The slot \code{normalizedTpmMatrix} of the provided \code{\link{CAGEset}}
-#' object will be occupied by normalized CAGE signal values per CTSS across all experiments,
-#' or with the raw tag counts (in case \code{method = "none"}).
+#' @return The slot `normalizedTpmMatrix` of the provided [`CAGEset`] object
+#' will be occupied by normalized CAGE signal values per CTSS across all
+#' experiments, or with the raw tag counts (in case `method = "none"`).
 #' 
 #' @references
 #' 
-#' Balwierz \emph{et al}. (2009) Methods for analyzing deep sequencing expression data:
-#' constructing the human and mouse promoterome with deepCAGE data, \emph{Genome Biology}
-#' \bold{10}(7):R79. 
+#' Balwierz _et al._ (2009) Methods for analyzing deep sequencing expression data:
+#' constructing the human and mouse promoterome with deepCAGE data, _Genome Biology_
+#' **10**(7):R79. 
 #' 
 #' @author Vanja Haberle
 #' 
-#' @seealso \code{\link{plotReverseCumulatives}}, \code{\link{CTSSnormalizedTpm}}
+#' @seealso [`plotReverseCumulatives`], [`CTSSnormalizedTpm`]
 #' 
 #' @family CAGEr object modifiers
 #' @family CAGEr normalised data functions
 #' 
 #' @examples 
+#' normalizeTagCount(exampleCAGEset, method = "simpleTpm")
 #' normalizeTagCount(exampleCAGEset, method = "powerLaw")
 #' normalizeTagCount(exampleCAGEexp, method = "simpleTpm")
+#' normalizeTagCount(exampleCAGEexp, method = "powerLaw")
 #' 
 #' @export
 
