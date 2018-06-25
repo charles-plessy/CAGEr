0861ebe30b8bcf4ac7d3d7b41f420e4890b0b290
Charles Plessy <plessy@riken.jp>
2018-06-25 15:15:55 +0900

    Markdownise while reading again.

--- R/ClusteringMethods.R
+++ R/ClusteringMethods.R
@@ -5,97 +5,95 @@
 #' @title Cluster CTSSs into tag clusters
 #' 
 #' @description Clusters individual CAGE transcription start sites (CTSSs) along
-#' the genome into tag clusters (TCs) using specified \emph{ab initio} method, or assigns
-#' them to predefined genomic regions.
+#' the genome into tag clusters (TCs) using specified _ab initio_ method, or
+#' assigns them to predefined genomic regions.
 #' 
-#' @param object A \code{\link{CAGEr}} object.
+#' @param object A [`CAGEr`] object.
 #' 
-#' @param threshold,nrPassThreshold Ignore CTSSs with signal \code{< threshold}
-#'        in \code{< nrPassThreshold} experiments.
+#' @param threshold,nrPassThreshold Ignore CTSSs with signal `< threshold`
+#'        in `< nrPassThreshold` experiments.
 #' 
-#' @param thresholdIsTpm Logical indicating if \code{threshold} is expressed in
-#'        raw tag counts (FALSE) or normalized signal (TRUE).
+#' @param thresholdIsTpm Logical indicating if `threshold` is expressed in
+#'        raw tag counts (`FALSE`) or normalized signal (`TRUE`).
 #' 
-#' @param method Method to be used for clustering: \code{"distclu"},
-#'        \code{"paraclu"} or \code{"custom"}.  See Details.
+#' @param method Method to be used for clustering: `"distclu"`,
+#'        `"paraclu"` or `"custom"`.  See Details.
 #' 
-#' @param maxDist Maximal distance between two neighbouring CTSSs for them to be part of the
-#'        same cluster.  Used only when \code{method = "distclu"}, otherwise ignored.
+#' @param maxDist Maximal distance between two neighbouring CTSSs for them to be
+#'        part of the same cluster.  Used only when `method = "distclu"`,
+#'        otherwise ignored.
 #' 
 #' @param removeSingletons Logical indicating if tag clusters containing only
-#'        one CTSS be removed.  Ignored when \code{method = "custom"}.
+#'        one CTSS be removed.  Ignored when `method = "custom"`.
 #' 
-#' @param keepSingletonsAbove Controls which singleton tag clusters will be removed.
-#'        When \code{removeSingletons = TRUE}, only singletons with signal
-#'        \code{< keepSingletonsAbove} will be removed.  Useful to prevent removing highly
-#'        supported singleton tag clusters.  Default value \code{Inf} results in removing all
-#'        singleton TCs when \code{removeSingletons = TRUE}.  Ignored when
-#'        \code{removeSingletons = FALSE} or \code{method = "custom"}.
+#' @param keepSingletonsAbove Controls which singleton tag clusters will be
+#'        removed.  When `removeSingletons = TRUE`, only singletons with signal
+#'        `< keepSingletonsAbove` will be removed.  Useful to prevent removing
+#'        highly supported singleton tag clusters.  Default value `Inf` results
+#'        in removing all singleton TCs when `removeSingletons = TRUE`.  Ignored
+#'        when `method = "custom"`.
 #' 
-#' @param minStability Minimal stability of the cluster, where stability is defined as ratio
-#'        between maximal and minimal density value for which this cluster is maximal scoring.
-#'        For definition of stability refer to Frith \emph{et al}., Genome Research, 2007.
-#'        Clusters with stability \code{< minStability} will be discarded.  Used only when
-#'        \code{method = "paraclu"}, otherwise ignored.
+#' @param minStability Minimal stability of the cluster, where stability is
+#'        defined as ratio between maximal and minimal density value for which
+#'        this cluster is maximal scoring.  For definition of stability refer to
+#'        Frith _et al._, Genome Research, 2007.  Clusters with stability
+#'        `< minStability` will be discarded.  Used only when `method = "paraclu"`.
 #' 
 #' @param maxLength Maximal length of cluster in base-pairs.  Clusters with length
-#'        \code{> maxLength} will be discarded.  Ignored when \code{method = "custom"}.
+#'        `> maxLength` will be discarded.  Ignored when `method = "custom"`.
 #' 
-#' @param reduceToNonoverlapping Logical, should smaller clusters contained within bigger
-#'        cluster be removed to make a final set of tag clusters non-overlapping. Used only
-#'        when \code{method = "paraclu"}.  See Details.
+#' @param reduceToNonoverlapping Logical, should smaller clusters contained
+#'        within bigger cluster be removed to make a final set of tag clusters
+#'        non-overlapping. Used only `method = "paraclu"`.
 #' 
 #' @param customClusters Genomic coordinates of predefined regions to be used to
-#'        segment the CTSSs.  The format is either a \code{\link{GRanges}}
-#'        object or a \code{data.frame} with the following columns: \code{chr}
-#'        (chromosome name), \code{start} (0-based start coordinate), \code{end}
-#'        (end coordinate), \code{strand} (either \code{"+"}, or \code{"-"}).
-#'        Used only when \code{method = "custom"}.
+#'        segment the CTSSs.  The format is either a [`GRanges`] object or a
+#'        [`data.frame`] with the following columns: `chr` (chromosome name),
+#'        `start` (0-based start coordinate), `end` (end coordinate), `strand`
+#'        (either `"+"`, or `"-"`).  Used only when `method = "custom"`.
 #' 
-#' @param useMulticore Logical, should multicore be used.  \code{useMulticore = TRUE}
+#' @param useMulticore Logical, should multicore be used.  `useMulticore = TRUE`
 #'        has no effect on non-Unix-like platforms.
 #' 
-#' @param nrCores Number of cores to use when \code{useMulticore = TRUE}.  Default value
-#'        \code{NULL} uses all detected cores.
+#' @param nrCores Number of cores to use when `useMulticore = TRUE`.  Default
+#'        value `NULL` uses all detected cores.
 #' 
-#' @details Two \emph{ab initio} methods for clustering TSSs along the genome are supported:
-#' \code{"distclu"} and \code{"paraclu"}.  \code{"distclu"} is an implementation of simple
-#' distance-based clustering of data attached to sequences, where two neighbouring TSSs are
+#' @details The `"distclu"` method is an implementation of simple distance-based
+#' clustering of data attached to sequences, where two neighbouring TSSs are
 #' joined together if they are closer than some specified distance (see
-#' \code{\link{distclu-functions}} for implementation details.  \code{"paraclu"} is an
-#' implementation of Paraclu algorithm for parametric clustering of data attached to
-#' sequences developed by M. Frith (Frith \emph{et al.}, Genome Research, 2007,
-#' \href{http://cbrc3.cbrc.jp/~martin/paraclu/}{http://cbrc3.cbrc.jp/~martin/paraclu/}).
-#' Since Paraclu finds #' clusters within clusters (unlike distclu), additional
-#' parameters (\code{removeSingletons}, \code{keepSingletonsAbove},
-#' \code{minStability}, \code{maxLength} and \code{reduceToNonoverlapping}) can
-#' be specified to simplify the output by discarding too small (singletons) or
-#' too big clusters, and to reduce the clusters to a final set of non-overlapping
-#' clusters.  Clustering is done for every CAGE dataset within the CAGEr object
-#' separately, resulting in a different set of tag clusters for every CAGE dataset. TCs from
-#' different datasets can further be aggregated into a single referent set of consensus
-#' clusters by calling the \code{\link{aggregateTagClusters}} function.
+#' [`distclu-functions`] for implementation details.
 #' 
-#' @return The slots \code{clusteringMethod}, \code{filteredCTSSidx} and \code{tagClusters} of
-#' the provided \code{\link{CAGEset}} object will be occupied by the information on method used
-#' for clustering, CTSSs included in the clusters and list of tag clusters per CAGE experiment,
-#' respectively.  To retrieve tag clusters for individual CAGE dataset use
-#' \code{\link{tagClusters}} function.
+#' `"paraclu"` is an implementation of Paraclu algorithm for parametric
+#' clustering of data attached to sequences (Frith _et al._, Genome Research,
+#' 2007).  Since Paraclu finds clusters within clusters (unlike distclu),
+#' additional parameters (`removeSingletons`, `keepSingletonsAbove`,
+#' `minStability`, `maxLength` and `reduceToNonoverlapping`) can be specified to
+#' simplify the output by discarding too small (singletons) or too big clusters,
+#' and to reduce the clusters to a final set of non-overlapping clusters.
 #' 
-#' In \code{\link{CAGEexp}} objects, the results will be stored as a
-#' \code{\link{GRangesList}} of \code{\link{TagClusters}} objects in the metadata
-#' slot \code{tagClusters}.  The \code{TagClusters} object will contain a
-#' \code{filteredCTSSidx} column if appropriate.  The clustering method name
-#' is saved in the metadata slot of the \code{GRangesList}.
+#' Clustering is done for every CAGE dataset within the CAGEr object separately,
+#' resulting in a different set of tag clusters for every CAGE dataset. TCs from
+#' different datasets can further be aggregated into a single referent set of
+#' consensus clusters by calling the [`aggregateTagClusters`] function.
 #' 
-#' @references Frith \emph{et al.} (2007) A code for transcription initiation in mammalian
-#' genomes, \emph{Genome Research} \bold{18}(1):1-12,
+#' @return The slots `clusteringMethod`, `filteredCTSSidx` and `tagClusters` of
+#' the provided [`CAGEset`] object will be occupied by the information on method
+#' used for clustering, CTSSs included in the clusters and list of tag clusters
+#' per CAGE experiment, respectively.  To retrieve tag clusters for individual
+#' CAGE dataset use the [`tagClusters()`] function.
+#' 
+#' In [`CAGEexp`] objects, the results will be stored as a `GRangesList` of
+#' [`TagClusters`] objects in the metadata slot `tagClusters`.  The
+#' `TagClusters` objects will contain a `filteredCTSSidx` column if appropriate.
+#' The clustering method name is saved in the metadata slot of the `GRangesList`.
+#' 
+#' @references Frith _et al._ (2007) A code for transcription initiation in
+#' mammalian genomes, _Genome Research_ **18**(1):1-12,
 #' (\href{http://www.cbrc.jp/paraclu/}{http://www.cbrc.jp/paraclu/}).
 #' 
 #' @author Vanja Haberle
 #' 
-#' @seealso \code{\link{tagClusters}}, \code{\link{aggregateTagClusters}} and
-#' \code{\link{CTSSclusteringMethod}}.
+#' @seealso [`tagClusters`], [`aggregateTagClusters`] and [`CTSSclusteringMethod`].
 #' 
 #' @family CAGEr object modifiers
 #' @family CAGEr clusters functions
