charles-plessy/CAGEr

commit 1b073f320012704938eeeb7e4e92459ecc0f6636

Transfer existense check to consensusClustersSE accessor.

R/GetMethods.R
@@ -588,8 +588,6 @@ setGeneric( "consensusClustersGR"
 
 setMethod( "consensusClustersGR", "CAGEexp"
          , function (object, sample, returnInterquantileWidth, qLow, qUp) {
-  if(is.null(experiments(object)$consensusClusters))
-    stop("No consensus clusters found.  See ", sQuote("?aggregateTagClusters"), " on how to create them.")
   cc <- rowRanges(consensusClustersSE(object))
   if(!is.null(sample)) {
     if (!is.null(qLow))
@@ -622,6 +620,8 @@ setGeneric("consensusClustersSE", function(object) standardGeneric("consensusClu
 #' @rdname consensusClusters
 
 setMethod("consensusClustersSE", "CAGEexp", function (object)
+  if(is.null(experiments(object)$consensusClusters))
+    stop("No consensus clusters found.  See ", sQuote("?aggregateTagClusters"), " on how to create them.")
   experiments(object)$consensusClusters)
 
 

commit 24fb49a1b0e6e1f61474b10ebbaf2d4f7e4ac229

Fix setMethod broken in commit 1b073f3 and update doc.

R/GetMethods.R
@@ -613,10 +613,11 @@ setGeneric("consensusClustersSE", function(object) standardGeneric("consensusClu
 
 #' @rdname consensusClusters
 
-setMethod("consensusClustersSE", "CAGEexp", function (object)
+setMethod("consensusClustersSE", "CAGEexp", function (object) {
   if(is.null(experiments(object)$consensusClusters))
     stop("No consensus clusters found.  See ", sQuote("?aggregateTagClusters"), " on how to create them.")
-  experiments(object)$consensusClusters)
+  experiments(object)$consensusClusters
+})
 
 
 #' @name consensusClustersDESeq2

man/consensusClusters.Rd
@@ -63,15 +63,10 @@ Extracts the information on consensus clusters from a \code{\link{CAGEr}}
 object.
 }
 \examples{
-consensusClustersGR(exampleCAGEexp, 2)
-ce <- quantilePositions(exampleCAGEexp, "consensusClusters")
-consensusClustersGR(ce, sample = 2
-                      , returnInterquantileWidth = TRUE
-                      , qLow = 0.1, qUp = 0.9)
-
 consensusClustersGR( exampleCAGEexp, sample = 2
                    , returnInterquantileWidth = TRUE
                    , qLow = 0.1, qUp = 0.9)
+
 }
 \seealso{
 \code{\link[=consensusClusters<-]{consensusClusters<-()}}