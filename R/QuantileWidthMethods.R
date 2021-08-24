3cc18abad5d5e56cd910ec95fe3ede14fc2b5c86
Charles Plessy <charles.plessy@oist.jp>
2021-08-24 12:56:34 +0000

    Refactor quantile functions in a single page; Dispatch only on CAGEexp

--- R/QuantileWidthMethods.R
+++ R/QuantileWidthMethods.R
@@ -5,38 +5,38 @@
 #' @title Determine CTSS quantile positions within clusters
 #' 
 #' @description Calculates the positions of \dQuote{upper} and \dQuote{lower}
-#' quantiles of CAGE signal along \emph{tag clusters} or
-#' \emph{consensus clusters} in each sample of a  CAGEr object.
+#' quantiles of CAGE signal along _tag clusters_ or _consensus clusters_ in each
+#' sample of a [`CAGEexp`] object.
 #' 
-#' @param object A \code{\link{CAGEr}} object.
+#' @param object A CAGEexp object.
 #' 
-#' @param clusters Either \code{tagClusters} or \code{consensusClusters}.
+#' @param clusters Either `tagClusters` or `consensusClusters`.
 #' 
 #' @param qLow,qUp Which \dQuote{lower} or \dQuote{upper} quantiles should be
-#'        calculated. Numeric vector of values in range \code{[0,1]}.
+#'        calculated. Numeric vector of values in range `[0,1]`.
 #' 
-#' @param useMulticore Logical, should multicore be used.
-#'        \code{useMulticore = TRUE} has only effect on Unix-like platforms.
+#' @param useMulticore Logical, should multicore be used.  `useMulticore = TRUE`
+#'        has only effect on Unix-like platforms.
 #' 
-#' @param nrCores Number of cores to use when \code{useMulticore = TRUE}.
-#'        Default value \code{NULL} uses all detected cores.
+#' @param nrCores Number of cores to use when `useMulticore = TRUE`.  Default
+#'        value `NULL` uses all detected cores.
 #' 
-#' @details From the 5' end the position, the position of a quantile \emph{q} is
-#' detemined as the first base in which of the cumulative expression is higher
-#' or equal to \emph{q%} of the total CAGE signal of that cluster.  Promoter
-#' \emph{interquantile width} is defined as the distance (in base pairs)
-#' between a \dQuote{lower} and an \dQuote{upper} quantile position.
+#' @details From the 5' end the position, the position of a quantile _q_ is
+#' determined as the first base in which of the cumulative expression is higher
+#' or equal to _q%_ of the total CAGE signal of that cluster.  Promoter
+#' _interquantile width_ is defined as the distance (in base pairs) between a
+#' \dQuote{lower} and an \dQuote{upper} quantile position.
 #' 
-#' @return Returns the objects, in wich the positions of the quantiles are defined
-#' reliatively to the start point of their cluster, for more efficient
-#' \code{Rle} compression.  The quantile data for \emph{tag clusters} are stored
-#' in the \code{TagClusters} objects directly.  The quantile data for
-#' \emph{consensus clusters} are stored in \code{\link{integer}} matrices named
-#' \dQuote{q_\emph{x}}, where \emph{x} represents the quantile (for instance,
-#' \code{q_0.1}), and these matrices are \emph{assays} of the
-#' \code{consensusClusters} \code{\link{RangedSummarizedExperiment}}.
+#' @return Returns the objects, in which the positions of the quantiles are
+#' defined relatively to the start point of their cluster, for more efficient
+#' `Rle` compression.  The quantile data for _tag clusters_ are stored in the
+#' `TagClusters` objects directly.  The quantile data for `consensus clusters`
+#' are stored in [`integer`] matrices named \dQuote{q_\emph{x}}, where \emph{x}
+#' represents the quantile (for instance, `q_0.1`), and these matrices are
+#' _assays_ of the `consensusClusters` [`RangedSummarizedExperiment`].
 #' 
 #' @author Vanja Haberle
+#' @author Charles Plessy
 #' 
 #' @family CAGEr object modifiers
 #' @family CAGEr clusters functions
@@ -58,38 +58,69 @@ setGeneric( "quantilePositions"
 
 #' @rdname quantilePositions
 
-setMethod( "quantilePositions", "CAGEr"
+setMethod( "quantilePositions", "CAGEexp"
          , function (object, clusters, qLow, qUp, useMulticore, nrCores) {
-	clusters <- match.arg(clusters)
-	message("\nGetting positions of quantiles within clusters...")
-	if (clusters == "tagClusters") {
-    ctss.clusters <- bplapply(sampleList(object),
-      function(s) {
+  clusters <- match.arg(clusters)
+  message("\nGetting positions of quantiles within clusters...")
+  if (clusters == "tagClusters") {
+    ctss.clusters <- bplapply(sampleList(object), function(s) {
         message("\t-> ", s)
         .get.quant.pos( cumsums  = CTSScumulativesTagClusters(object, s)
                       , clusters = tagClustersGR(object, s)
                       , q        = c(qLow, qUp))
       },
       BPPARAM = CAGEr_Multicore(useMulticore, nrCores))
-	  ctss.clusters <- GRangesList(ctss.clusters)
-		if(inherits(object, "CAGEexp")) {
-		  tagClustersGR(object) <- ctss.clusters
-		} else stop("Unsupported CAGEr class.")
+    ctss.clusters <- GRangesList(ctss.clusters)
+		tagClustersGR(object) <- ctss.clusters
 	} else if (clusters == "consensusClusters"){
-    cons.clusters.l <- bplapply(sampleList(object),
-      function(s) {
+    cons.clusters.l <- bplapply(sampleList(object), function(s) {
         message("\t-> ", s)
         .get.quant.pos( cumsums  = CTSScumulativesCC(object, s)
                       , clusters = consensusClustersGR(object)
                       , q        = c(qLow, qUp))
       },
       BPPARAM = CAGEr_Multicore(useMulticore, nrCores))
-    
-		if(inherits(object, "CAGEexp")) {
-      for (qName in paste("q", c(qLow, qUp), sep = "_")) {
-        assays(consensusClustersSE(object), withDimnames=FALSE)[[qName]] <-
-          DataFrame(lapply(cons.clusters.l, function(gr) mcols(gr)[,qName]))}
-		} else stop("Unsupported CAGEr class.")
+    for (qName in paste("q", c(qLow, qUp), sep = "_")) {
+      assays(consensusClustersSE(object), withDimnames=FALSE)[[qName]] <-
+        DataFrame(lapply(cons.clusters.l, function(gr) mcols(gr)[,qName]))}
 	}
-      object
+  object
 })
+
+#' Get quantile positions
+#' 
+#' Private function that calculates position of quantiles for CTSS clusters
+#' based on distribution of tags within the clusters.
+#' 
+#' @param cumsums Named list of vectors containing cumulative sum for each
+#'        cluster (returned by the `CTSScumulativesTagClusters` or
+#'        `CTSScumulativesCC` function).
+#' @param clusters [`TagClusters`] or [`ConsensusClusters`] object representing
+#'        tag clusters or consensus clusters.
+#' @param q desired quantiles - single value or a vector of values.
+#' 
+#' @return Returns the `clusters` object with one more metadata column per value
+#' in `q`, containing `Rle` integers giving the relative distance of the
+#' quantile boundaries to the start position.
+#' 
+#' @examples 
+#' \dontrun{ #because it runs through quantilePositions() anyway.
+#' cumsums  <- CTSScumulativesTagClusters(exampleCAGEexp, 1)
+#' clusters <- tagClustersGR(exampleCAGEexp, 1)
+#' CAGEr:::.get.quant.pos(cumsums, clusters, c(.2, .8))
+#' }
+
+#' @rdname QuantileWidthFunctions
+
+.get.quant.pos <- function(cumsums, clusters, q) {
+  getQuantilepos <- Vectorize(vectorize.args = "cumsum", function(q, cumsum) {
+    cumsum <- decode(cumsum)
+    max <- tail(cumsum,1)   # Max is last element since we x is a cumulative sums.
+    treshold <- max * q
+    which.max(cumsum >= treshold)
+  })
+  cluster.q <- lapply(q, getQuantilepos, cumsums)
+  names(cluster.q) = paste('q_', q, sep = '')
+  mcols(clusters)[, names(cluster.q)] <- DataFrame(lapply(cluster.q, Rle))
+  clusters
+}
