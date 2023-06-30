28e1160b65bd4427fd49fa17d37812894e831580
Charles Plessy <charles.plessy@oist.jp>
2023-06-30 17:34:07 +0900

    Correct cumulative sums computation.
    The sapply function would skip bases with zero expression, causing further quantile computations to be incorrect.
    
    I found this bug after seeing that the dominant peak was often outside the 10–90% quantile range…

--- R/GetMethods.R
+++ R/GetMethods.R
@@ -642,7 +642,7 @@ bioC2_cc_iqw <- function(o, clusters, ctss, qLow = 0.1, qUp = 0.9,
     cluster_start_idx <- cumsum(c(1, head(rl, -1))) # Where each run starts
     grouped_scores <- extractList(score(ctss), o)
     ##
-    grouped_scores_cumsum <- sapply(grouped_scores, cumsum)
+    grouped_scores_cumsum <- .getCumsum(ctss, clusters)
     
     if (return_iqw) {
       qLowName <- paste0("q_", qLow)
