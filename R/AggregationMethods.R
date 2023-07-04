c9b8305afa9e1b66d8df34bf2ce39fc6bddb976e
Charles Plessy <charles.plessy@oist.jp>
2023-07-04 19:13:52 +0900

    Fix .rowsumAsMatrix for the case where some CTSS are not in clusters.

--- R/AggregationMethods.R
+++ R/AggregationMethods.R
@@ -192,11 +192,11 @@ setMethod( ".CCtoSE"
       se <- se[rowSums(DelayedArray(assays(se)[["normalizedTpmMatrix"]])) > tpmThreshold,]
     
     .rowsumAsMatrix <- function(DF, names) {
-      rs <- rowsum(as.matrix(DelayedArray(DF)), as.factor(names), reorder = FALSE)
-      if (rownames(rs)[1] == "") # If some CTSS were not in clusters
-        rs <- rs[-1, , drop = FALSE]
-      rs
-      }
+      # First, remove CTSS that do not match clusters
+      DF <- DF[names != "",]
+      names <- names[names != ""]
+      rowsum(as.matrix(DelayedArray(DF)), as.factor(names), reorder = FALSE)
+    }
     
     counts <- .rowsumAsMatrix(assays(se)[["counts"]], rowRanges(se)$cluster)
     norm   <- .rowsumAsMatrix(assays(se)[["normalizedTpmMatrix"]], rowRanges(se)$cluster)
